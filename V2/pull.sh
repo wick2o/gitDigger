51f9eebff37e408db59a829c5b75992d13cd5b34
Jaime Filson <wick2o@gmail.com>
2013-10-20 21:29:08 -0400

    fix pull.sh

--- V2/pull.sh
+++ V2/pull.sh
@@ -10,15 +10,11 @@ echo "Dumping:"
 
 echo "    github usernames..." 
 mysql -h$db_host -u$db_user -p$db_pass -D$db_name -N -e 'SELECT DISTINCT username FROM projects ORDER BY username ASC' > github-usernames.txt
+tar -czf github-usernames.tar.gz github-usernames.txt
+rm github-usernames.txt
 
 echo "    github repositores..."
 mysql -h$db_host -u$db_user -p$db_pass -D$db_name -N -e 'SELECT DISTINCT name FROM projects ORDER BY username ASC' > github-repositories.txt
-
-
-echo "Compressing data..."
-
-tar -czf github_users.tar.gz github_users.txt
-tar -czf github_projects.tar.gz github_projects.txt
-
-echo "Cleaning up..."
+tar -czf github-repositories.tar.gz github-repositories.txt
+rm github-repositories.txt
 
